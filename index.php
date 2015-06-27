16af397b00e78b810053df2e903fe79dd2e766c7
Jacob Reed <jacobreedbyui@gmail.com>
2015-06-27 16:59:06 -0700

    Included some comments
    Contemplating some changes as well.

--- index.php
+++ index.php
@@ -18,12 +18,15 @@ and open the template in the editor.
 
 <form method="post">
     <select name="selectFamily">
+        <!-- These options will be pulled from information input by user on "families page" -->
+        <!-- Possibly to be made as a text input... as it doesn't need much other than the actual name -->
         <option value="1">Example Family 1</option>
         <option value="2">Example Family 2</option>
         <option value="3">Example Family 3</option>
     </select>
     <hr>
-    
+        
+        <!-- Both date and time functions incompatible with Firefox browsers - Sorry! Use virtually any other brower. -->
 <h3>Date</h3>
 
     <input type="date" name="date"/>
@@ -33,8 +36,8 @@ and open the template in the editor.
 
     <input type="time" name="time">
     <br><br>
-    <button onclick="store()" type="button">Save Appointment</button>
 </form>
+<button onclick="store()" type="button">Save Appointment</button>
         <hr>
 
 <h3>Countdown</h3>
