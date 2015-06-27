087c4103d0ee0fe9ea8f6e476eb8326fa159d582
Jacob Reed <jacobreedbyui@gmail.com>
2015-06-27 15:40:27 -0700

    Rough code of appointment scheduler
    Work in progress.

--- index.php
+++ index.php
@@ -10,8 +10,36 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
+        <!-- This is just a very rough sketch of some of the key features of our appointment scheduler -->
+<h1>Schedule Appointment</h1>
+
+<h3>Family</h3>
+
+<form method="post">
+    <select name="selectFamily">
+        <option value="1">Example Family 1</option>
+        <option value="2">Example Family 2</option>
+        <option value="3">Example Family 3</option>
+    </select>
+    <hr>
+    
+<h3>Date</h3>
+
+    <input type="date" name="date"/>
+    <hr>
+    
+<h3>Time</h3>
+
+    <input type="time" name="time">
+    <br><br>
+    <button onclick="store()" type="button">Save Appointment</button>
+</form>
+        <hr>
+
+<h3>Countdown</h3>
+
+<div id="countdown"></div>
+
     </body>
 </html>
