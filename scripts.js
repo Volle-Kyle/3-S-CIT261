379676333b8398a5ce48b6fc6670362a83f71a28
Jacob Reed <jacobreedbyui@gmail.com>
2015-07-18 14:39:31 -0700

    Fixed table centering and typo

--- scripts.js
+++ scripts.js
@@ -161,11 +161,11 @@ function addMember() {
 
 function memberInfo() {
     var jones = JSON.parse(localStorage.getItem('jones'));
-    var output = "<table align='block'><tr><td><p class='fancy-font'>Name:</p></td><td>" + jones.members[0].name + "</td></tr>"
+    var output = "<table class='center'><tr><td><p class='fancy-font'>Name:</p></td><td>" + jones.members[0].name + "</td></tr>"
                + "<tr><td><p class='fancy-font'>Position In Family:</p></td><td>" + jones.members[0].position + "</td></tr>"
                + "<tr><td><p class='fancy-font'>Birthday:</p></td><td>" + jones.members[0].birthday + "</td></tr>"
-               + "<tr><td><p class='fancy-font'>Notes:</p></td><td>" + jones.members[0].notes + "</td></tr>"
-               + "<tr><td><center><input type='button' style='width: 200px' onclick='displayFamily()' class='button' value='Back'/><center><</td></table>";
+               + "<tr><td><p class='fancy-font'>Notes:</p></td><td>" + jones.members[0].notes + "</td></tr></table>"
+               + "<tr><center><input type='button' style='width: 200px' onclick='displayFamily()' class='button' value='Back'/><center>";
     document.getElementById("family-display").innerHTML = output;
 }
 
